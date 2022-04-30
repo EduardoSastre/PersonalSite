8a4da931f4370e52fc2fed622182447f788d3785
Eduardo Sastre Justiniano <99148954+EduardoSastre@users.noreply.github.com>
2022-04-29 22:29:13 -0500

    Add files

---
+++ js/animation.js
@@ -0,0 +1,104 @@
+gsap.registerPlugin(ScrollTrigger);
+
+ww = window.innerWidth;
+wh = window.innerHeight;
+
+var tl = gsap.timeline({repeat:-1, yoyo: true});
+
+tl.to('.Top-Container__TopCloud', {
+  duration: 20,
+  x: ww*-0.55
+});
+
+tl.to('.Top-Container__BottomCloud', {
+  duration: 20,
+  x: ww*0.60
+}, 0);
+
+tl.to('.Top-Container__BottomCloud2', {
+  duration: 20,
+  x: ww*-0.55
+}, 0);
+
+gsap.from('h1', {
+  duration: 2,
+  opacity: 0
+}, 0);
+
+gsap.from('.Top-Container__Description', {
+  delay: 1,
+  duration: 2,
+  opacity: 0
+}, 0);
+
+gsap.from('p', {
+  duration: 2,
+  y: +50
+}, 0);
+
+gsap.from('.skill-row1', {
+  x: -0.10*ww,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.skill-row1',
+    start: "center 80%",
+  }
+});
+
+gsap.from('.skill-row2', {
+  x: 0.10*ww,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.skill-row2',
+    start: "center 80%",
+  }
+});
+
+gsap.from('.profile__MyImg', {
+  scale: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.profile__MyImg',
+    start: "center 80%",
+  }
+});
+
+gsap.from('.profile__title', {
+  delay: 1,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.profile__MyImg',
+    start: "center 80%",
+  }
+});
+
+gsap.from('.profile__description', {
+  delay: 1.5,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.profile__MyImg',
+    start: "center 80%",
+  }
+});
+
+gsap.from('.skills__title', {
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.skills__title',
+    start: "center 80%",
+  }
+});
+
+gsap.from('.contact-me', {
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.skill-row2',
+    start: "center center",
+  }
+});
